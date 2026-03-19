e4789c31557bb2bfe0cc46f3541623c94b6d8ebe
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-03-19 19:12:44 +0600

    changed code

--- src/Component/FeatureSection.jsx
+++ src/Component/FeatureSection.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import banner from "../../src/assets/feature.png"; //
+import banner from "../../src/assets/images.jfif"; //
 
 const FeatureSection = () => {
   return (
-    <section className="py-20 bg-base-200">
+    <section className="py-16 bg-base-200">
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
 
         {/* Image */}
-        <div className="flex justify-center">
+        <div className="flex justify-center h-full w-full">
           <img
             src={banner}
             alt="Premium Watch"
@@ -48,51 +48,8 @@ const FeatureSection = () => {
           </button>
         </div>
       </div>
-      <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2 gap-12 items-center mt-20">
-
-       
-
-        {/* Content */}
-        <div>
-          <h2 className="text-3xl md:text-4xl font-bold leading-tight">
-            Designed for <span className="text-red-500">Modern Lifestyle</span>
-          </h2>
-
-          <p className="mt-4 text-gray-600">
-            Experience precision, durability, and elegance combined into one
-            premium timepiece. Built for those who value both style and
-            performance.
-          </p>
-
-          {/* Features */}
-          <ul className="mt-6 space-y-4">
-            <li className="flex items-center gap-3">
-              <span className="text-red-500 text-xl">✔</span>
-              High-quality stainless steel body
-            </li>
-            <li className="flex items-center gap-3">
-              <span className="text-red-500 text-xl">✔</span>
-              Water resistant up to 50 meters
-            </li>
-            <li className="flex items-center gap-3">
-              <span className="text-red-500 text-xl">✔</span>
-              Long-lasting battery performance
-            </li>
-          </ul>
-
-          <button className="btn btn-error mt-8 text-white">
-            Learn More
-          </button>
-        </div>
-         {/* Image */}
-        <div className="flex justify-center">
-          <img
-            src={banner}
-            alt="Premium Watch"
-            className=" rounded-xl shadow-xl hover:scale-105 transition duration-300"
-          />
-        </div>
-      </div>
+      
+      
     </section>
   );
 };
